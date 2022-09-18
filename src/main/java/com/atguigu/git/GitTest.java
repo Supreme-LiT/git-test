c5387e440b493bc8f5d5505a12675e1f3f7c746d
LT <2025869@qq.com>
2022-09-18 19:48:55 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @ClassName GitTest
+ * @Description TODO
+ * @Author LT
+ * @DateTime 2022/9/18 19:44
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,Git");
+    }
+}
